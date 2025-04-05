2f4bf85cd5267b634895f4439f317c10fa8d9148
varshadhanotiya <varshadhanotiya7026@gmail.com>
2025-04-05 18:37:59 +0530

    map restaurent list

--- App.js
+++ App.js
@@ -1,152 +1,428 @@
-import React from 'react'
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <div className='logo-container'>
-                <img className="logo" src="https://imgs.search.brave.com/wG0wX0fVuWOQC1dJ_VXwXmM4pP2PtbF-EUv4cILhQpg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bG9nb2Rlc2lnbi5u/ZXQvbG9nby1uZXcv/Z3JlZW4tYW5kLWJl/aWdlLWZvb2QtdHJ1/Y2stODg0OWxkLnBu/Zz9ud209MSZud3M9/MSZpbmR1c3RyeT1m/b29kJnR4dF9rZXl3/b3JkPUFsbA" />
-            </div>
-            <div className='nav-items'>
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://imgs.search.brave.com/wG0wX0fVuWOQC1dJ_VXwXmM4pP2PtbF-EUv4cILhQpg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bG9nb2Rlc2lnbi5u/ZXQvbG9nby1uZXcv/Z3JlZW4tYW5kLWJl/aWdlLWZvb2QtdHJ1/Y2stODg0OWxkLnBu/Zz9ud209MSZud3M9/MSZpbmR1c3RyeT1m/b29kJnR4dF9rZXl3/b3JkPUFsbA"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 const RestaurantCard = (props) => {
-    const {resData} = props
-    // console.log("props aree----", props, resName)
-    return (
-        <div className='res-card' style={{backgroundColor : "#f0f0f0"}}>
-            <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+ resData.info.cloudinaryImageId} alt="res-logo" />
-            <h3>{resData.info.name}</h3>
-            <h4>{resData.info.cuisines.join(", ")}</h4>
-            <h4>{resData.info.avgRating} stars</h4>
-            <h4>{resData.info.costForTwo}</h4>
-            <h4>{resData.info.sla.deliveryTime} minutes</h4>
-        </div>
-    );
-}
+  const { resData } = props;
 
-const resObj = {
-    "info": {
-      "id": "671928",
-      "name": "KFC",
-      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/377e9327-3ca7-48ac-b36c-96f5bf6e186a_671928.JPG",
-      "locality": "7th Block",
-      "areaName": "Koramangla",
-      "costForTwo": "₹400 for two",
-      "cuisines": [
-        "Burgers",
-        "Fast Food",
-        "Rolls & Wraps"
-      ],
-      "avgRating": 4.3,
-      "parentId": "547",
-      "avgRatingString": "4.3",
-      "totalRatingsString": "4.3K+",
-      "sla": {
-        "deliveryTime": 21,
-        "lastMileTravel": 1.2,
-        "serviceability": "SERVICEABLE",
-        "slaString": "20-25 mins",
-        "lastMileTravelString": "1.2 km",
-        "iconType": "ICON_TYPE_EMPTY"
+  const { id, cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
+    resData.info;
+  // console.log("props aree----", props, resName)
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+        alt="res-logo"
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.deliveryTime} minutes</h4>
+    </div>
+  );
+};
+
+const resList = [
+  {
+    info: {
+      id: "426776",
+      name: "Theobroma",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/6/ef26de8c-72a5-494b-9662-e4c1f51e26bc_426776.jpg",
+      locality: "7th Block",
+      areaName: "Koramangala",
+      costForTwo: "₹400 for two",
+      cuisines: ["Desserts", "Bakery", "Beverages"],
+      avgRating: 4.7,
+      parentId: "1040",
+      avgRatingString: "4.7",
+      totalRatingsString: "6.0K+",
+      sla: {
+        deliveryTime: 19,
+        lastMileTravel: 1.3,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "1.3 km",
+        iconType: "ICON_TYPE_EMPTY",
       },
-      "availability": {
-        "nextCloseTime": "2025-04-06 02:00:00",
-        "opened": true
+      availability: {
+        nextCloseTime: "2025-04-05 23:45:00",
+        opened: true,
       },
-      "badges": {
-        "imageBadges": [
+      badges: {
+        imageBadges: [
           {
-            "imageId": "bolt/bolt%206.png",
-            "description": "bolt!"
-          }
-        ]
+            imageId: "bolt/bolt%206.png",
+            description: "bolt!",
+          },
+          {
+            imageId: "Rxawards/_CATEGORY-Desserts.png",
+            description: "Delivery!",
+          },
+        ],
       },
-      "isOpen": true,
-      "type": "F",
-      "badgesV2": {
-        "entityBadges": {
-          "imageBased": {
-            "badgeObject": [
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
               {
-                "attributes": {
-                  "description": "bolt!",
-                  "imageId": "bolt/bolt%206.png"
-                }
-              }
-            ]
+                attributes: {
+                  description: "bolt!",
+                  imageId: "bolt/bolt%206.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Desserts.png",
+                },
+              },
+            ],
           },
-          "textBased": {
-            
-          },
-          "textExtendedBadges": {
-            
-          }
-        }
+          textBased: {},
+          textExtendedBadges: {},
+        },
       },
-      "aggregatedDiscountInfoV3": {
-        "header": "ITEMS",
-        "subHeader": "AT ₹59"
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        discountTag: "FLAT DEAL",
       },
-      "differentiatedUi": {
-        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-        "differentiatedUiMediaDetails": {
-          "lottie": {
-            
-          },
-          "video": {
-            
-          }
-        }
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
       },
-      "reviewsSummary": {
-        
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
       },
-      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-      "restaurantOfferPresentationInfo": {
-        
-      },
-      "externalRatings": {
-        "aggregatedRating": {
-          "rating": "--"
-        }
-      },
-      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
     },
-    "analytics": {
-      "context": "seo-data-f6e3890d-07cf-48e1-bd45-b6b2d72cf9e9"
+    analytics: {
+      context: "seo-data-f6e3890d-07cf-48e1-bd45-b6b2d72cf9e9",
     },
-    "cta": {
-      "link": "https://www.swiggy.com/city/bangalore/kfc-7th-block-koramangla-rest671928",
-      "type": "WEBLINK"
-    }
-  };
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/theobroma-7th-block-koramangala-rest426776",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "671928",
+      name: "KFC",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/377e9327-3ca7-48ac-b36c-96f5bf6e186a_671928.JPG",
+      locality: "7th Block",
+      areaName: "Koramangla",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Fast Food", "Rolls & Wraps"],
+      avgRating: 4.3,
+      parentId: "547",
+      avgRatingString: "4.3",
+      totalRatingsString: "4.3K+",
+      sla: {
+        deliveryTime: 21,
+        lastMileTravel: 1.2,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-04-06 02:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "bolt/bolt%206.png",
+            description: "bolt!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "bolt/bolt%206.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹59",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-f6e3890d-07cf-48e1-bd45-b6b2d72cf9e9",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/kfc-7th-block-koramangla-rest671928",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "23678",
+      name: "McDonald's",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/20/9cf0f783-d73f-4cb8-993e-8031bf95f039_23678.jpg",
+      locality: "5th Block",
+      areaName: "Koramangala",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Beverages", "Cafe", "Desserts"],
+      avgRating: 4.4,
+      parentId: "630",
+      avgRatingString: "4.4",
+      totalRatingsString: "64K+",
+      sla: {
+        deliveryTime: 19,
+        lastMileTravel: 1.2,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "1.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-04-06 02:45:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "bolt/bolt%206.png",
+            description: "bolt!",
+          },
+          {
+            imageId: "Ratnesh_Badges/Rx_Awards_2025/Bolt.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "bolt/bolt%206.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Ratnesh_Badges/Rx_Awards_2025/Bolt.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹80 OFF",
+        subHeader: "ABOVE ₹349",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-f6e3890d-07cf-48e1-bd45-b6b2d72cf9e9",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/mcdonalds-5th-block-koramangala-rest23678",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "65797",
+      name: "Leon's - Burgers & Wings (Leon Grill)",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/14/33bfb682-d5fa-4054-9e2c-31911e34ebf6_65797.jpg",
+      locality: "Koramangala",
+      areaName: "Koramangala",
+      costForTwo: "₹300 for two",
+      cuisines: ["American", "Snacks", "Turkish", "Portuguese", "Continental"],
+      avgRating: 4.5,
+      parentId: "371281",
+      avgRatingString: "4.5",
+      totalRatingsString: "56K+",
+      sla: {
+        deliveryTime: 19,
+        lastMileTravel: 1.3,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "1.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-04-06 04:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "bolt/bolt%206.png",
+            description: "bolt!",
+          },
+          {
+            imageId: "Rxawards/_CATEGORY-Burger.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "bolt/bolt%206.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Burger.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.5",
+          ratingCount: "4.7K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-f6e3890d-07cf-48e1-bd45-b6b2d72cf9e9",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/leons-burgers-and-wings-leon-grill-koramangala-rest65797",
+      type: "WEBLINK",
+    },
+  },
+];
 
-const Body = () =>{
-    return(
-        <div className='body'>
-            <div className='search'>Search</div>
-            <div className='res-container'>
-                <RestaurantCard resData={resObj} />
-            </div>
-        </div>
-    );
-}
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resList.map((restaurent) => (
+            <RestaurantCard key={restaurent.info.id} resData={restaurent} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 function AppLayout() {
   return (
     <div className="app">
-        <Header />
-        <Body />
+      <Header />
+      <Body />
     </div>
   );
 }
