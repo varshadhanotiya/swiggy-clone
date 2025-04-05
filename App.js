e2bee7b993a43436673cff2ea1e0c81b5a0a966d
varshadhanotiya <varshadhanotiya7026@gmail.com>
2025-04-05 18:04:37 +0530

    make restaurent card dynamic

--- App.js
+++ App.js
@@ -20,26 +20,123 @@ const Header = () => {
 }
 
 const RestaurantCard = (props) => {
-    const {cuisine, resName} = props
+    const {resData} = props
     // console.log("props aree----", props, resName)
     return (
         <div className='res-card' style={{backgroundColor : "#f0f0f0"}}>
-            <img className="res-logo" src="https://imgs.search.brave.com/IOlz2aQiGVjOLJGY6jgOE0WHKcCUudgJHbAWBPltKEE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9y/aXNvdHRvLXdpdGgt/YXNwYXJhZ3VzLWJl/YW5zLXp1Y2NoaW5p/LWdyZWVuLXBlYXNf/MjgyOS04Mjc2Lmpw/Zz9zZW10PWFpc19o/eWJyaWQ" alt="res-logo" />
-            <h3>{resName}</h3>
-            <h4>{cuisine}</h4>
-            <h4>4.4 stars</h4>
-            <h4>38 minutes</h4>
+            <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+ resData.info.cloudinaryImageId} alt="res-logo" />
+            <h3>{resData.info.name}</h3>
+            <h4>{resData.info.cuisines.join(", ")}</h4>
+            <h4>{resData.info.avgRating} stars</h4>
+            <h4>{resData.info.costForTwo}</h4>
+            <h4>{resData.info.sla.deliveryTime} minutes</h4>
         </div>
     );
 }
+
+const resObj = {
+    "info": {
+      "id": "671928",
+      "name": "KFC",
+      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/377e9327-3ca7-48ac-b36c-96f5bf6e186a_671928.JPG",
+      "locality": "7th Block",
+      "areaName": "Koramangla",
+      "costForTwo": "₹400 for two",
+      "cuisines": [
+        "Burgers",
+        "Fast Food",
+        "Rolls & Wraps"
+      ],
+      "avgRating": 4.3,
+      "parentId": "547",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "4.3K+",
+      "sla": {
+        "deliveryTime": 21,
+        "lastMileTravel": 1.2,
+        "serviceability": "SERVICEABLE",
+        "slaString": "20-25 mins",
+        "lastMileTravelString": "1.2 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2025-04-06 02:00:00",
+        "opened": true
+      },
+      "badges": {
+        "imageBadges": [
+          {
+            "imageId": "bolt/bolt%206.png",
+            "description": "bolt!"
+          }
+        ]
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            "badgeObject": [
+              {
+                "attributes": {
+                  "description": "bolt!",
+                  "imageId": "bolt/bolt%206.png"
+                }
+              }
+            ]
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "ITEMS",
+        "subHeader": "AT ₹59"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      "context": "seo-data-f6e3890d-07cf-48e1-bd45-b6b2d72cf9e9"
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/bangalore/kfc-7th-block-koramangla-rest671928",
+      "type": "WEBLINK"
+    }
+  };
 
 const Body = () =>{
     return(
         <div className='body'>
             <div className='search'>Search</div>
             <div className='res-container'>
-                <RestaurantCard resName="First Outlet" cuisine="Biryani, North Indian"/>
-                <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
+                <RestaurantCard resData={resObj} />
             </div>
         </div>
     );
