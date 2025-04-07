34a59281c219c554a8320036e6e7e67ebb142b38
varshadhanotiya <varshadhanotiya7026@gmail.com>
2025-04-07 15:50:46 +0530

    filter based on ratings

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
 
 const Body = () => {
+  const [listOfRestaurents, setListOfRestaurents] = useState(resList);
+
+  const filterTopRated = () => {
+    const filteredList = listOfRestaurents.filter(
+      (restaurant) => restaurant.info.avgRating > 4.5
+    );
+    setListOfRestaurents(filteredList);
+  };
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={filterTopRated}>
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurent) => (
-          <RestaurantCard key={restaurent.info.id} resData={restaurent} />
-        ))}
+        {listOfRestaurents.length > 0 ? (
+          listOfRestaurents.map((restaurant) => (
+            <RestaurantCard
+              key={restaurant.info.id}
+              resData={restaurant}
+            />
+          ))
+        ) : (
+          <p>No restaurants to display</p>
+        )}
       </div>
     </div>
   );
