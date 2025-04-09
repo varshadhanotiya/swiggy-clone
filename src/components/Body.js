6a34867ae6012830548f5e3031ed122791d99ecf
varshadhanotiya <varshadhanotiya7026@gmail.com>
2025-04-10 00:25:15 +0530

    update swiggy api url

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,7 +16,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=12.9352403&lng=77.624532&carousel=true&third_party_vendor=1"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  //if no dependency then useEffect is called on every render
+  //if dependency array is empty = [] => useEffect is called on initial render()
+  // if dependency array is (btnNameReact) => called everytime btnNameReact is updated
+  useEffect(() => {
+    console.log("useEffect called");
+  }, [btnNameReact]);
 
   return (
     <div className="header">
